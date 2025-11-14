c2488276183f3281990f437666fe19aee6eaa307
Prama Kusuma <prama@daya.ai>
2025-11-14 16:55:08 +0800

    fix: use geolocatator as default location manager

--- batagor/Model/PlacemarkInfo.swift
+++ batagor/Model/PlacemarkInfo.swift
@@ -13,26 +13,32 @@ struct PlacemarkInfo {
     var name: String?
     var thoroughfare: String?
     var locality: String?
+    var subLocality: String?
     var administrativeArea: String?
     var postalCode: String?
     var country: String?
+    var areasOfInterest: [String]?
     
     init(from placemark: CLPlacemark) {
         self.name = placemark.name
         self.thoroughfare = placemark.thoroughfare
+        self.subLocality = placemark.subLocality
         self.locality = placemark.locality
         self.administrativeArea = placemark.administrativeArea
         self.postalCode = placemark.postalCode
         self.country = placemark.country
+        self.areasOfInterest = placemark.areasOfInterest
     }
     
     init(from placemark: MKPlacemark) {
         self.name = placemark.name
         self.thoroughfare = placemark.thoroughfare
         self.locality = placemark.locality
+        self.subLocality = placemark.subLocality
         self.administrativeArea = placemark.administrativeArea
         self.postalCode = placemark.postalCode
         self.country = placemark.country
+        self.areasOfInterest = placemark.areasOfInterest
     }
     
     var displayName: String {
@@ -42,8 +48,14 @@ struct PlacemarkInfo {
         if let thoroughfare = thoroughfare {
             return thoroughfare
         }
+        if let subLocality = subLocality {
+            return subLocality
+        }
         if let locality = locality {
             return locality
+        }
+        if let areas = areasOfInterest, !areas.isEmpty, let firstArea = areas.first {
+            return firstArea
         }
         return "Unknown Location"
     }
